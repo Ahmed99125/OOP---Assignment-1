f9e2e8165539ef6431e4b919f9a20d145bcd3737
mohamed20047 <88450499+mohamed20047@users.noreply.github.com>
2023-10-05 20:15:08 +0300

    first sex filters

--- main.cpp
+++ main.cpp
@@ -41,3 +41,74 @@ void saveGreyImage() {
 void display() {
     int option = -1;
 }
+
+
+
+void Filter1(){ // Black and White Image
+    for(int i=0; i<SIZE; ++i) {
+        for(int j=0; j<SIZE; ++j) {
+            if(image[i][j] <= 127)
+                image[i][j] = 0;
+            else if(image[i][j] > 127)
+                image[i][j] = 255;
+        }
+    }
+}
+void Filter2(){ // : Invert Image
+    for(int i=0; i<SIZE; ++i) {
+        for(int j=0; j<SIZE; ++j) {
+            image[i][j] = 255 - image[i][j];
+        }
+    }
+}
+void Filter3(){ // Merge Images
+    int matrix2[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0 ,0}};
+    for(int i=0; i<SIZE; ++i) {
+        for(int j=0; j<SIZE; ++j) {
+            image[i][j] = (image[i][j] + matrix2[i][j]) / 2;
+        }
+    }
+}
+void Filter4(){ //Flip Image
+    for(int i=0; i<SIZE; ++i) {
+        for(int j=0; j<SIZE; ++j) {
+            int temp = image[x][y];
+            image[x][y]=image[SIZE-i][j]
+            matrix[SIZE-i][j]=temp;
+        }
+    }
+
+}
+void Filter5(){ // Rotate Image
+    int matrix2[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0 ,0}};
+
+// rotate 90 deg
+
+    for(int i=0; i<SIZE; ++i) {
+        for(int j=0; j<SIZE; ++j) {
+            image[i][j] = matrix2[SIZE - 1 - j][i];
+        }
+    }
+
+// rotate 180 deg
+
+    for(int i=SIZE - 1; i>-1; --i) {
+        for(int j=SIZE - 1; j>-1; --j) {
+            image[i][j] = matrix2[SIZE - i][SIZE - j];
+        }
+    }
+
+// rotate 270 deg
+    //! run both 90 deg then 180 deg
+
+}
+void Filter6(){ // Darken and Lighten Image
+    for(int i=0; i<SIZE; ++i) {
+        for(int j=0; j<SIZE; ++j) {
+            // plus for ligher
+            image[i][j] = image[i][j] + floor(.5 * image[i][j]);
+            // minus for darker
+            image[i][j] = image[i][j] - floor(.5 * image[i][j]);
+        }
+    }
+}
