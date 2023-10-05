ef459e13903f59f1bf3b2fa9f6371ec286dc6548
Ahmed99125 <at99125@gmail.com>
2023-10-05 20:20:54 +0300

    list done

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ void saveGreyImage();
 void display();
 
 int main() {
-    display()
+    display();
 }
 
 
@@ -39,5 +39,52 @@ void saveGreyImage() {
 }
 
 void display() {
+    cout << "Please enter file name of the image to process:" << endl;
+    loadGreyImage();
+
+    cout << "Please select a filter to apply or 0 to exit:" << endl;
+    cout << "1- Black & White Filter" << endl;
+    cout << "2- Invert Filter" << endl;
+    cout << "3- Merge Filter" << endl;
+    cout << "4- Flip Filter" << endl;
+    cout << "5- Darken and Lighten Image" << endl;
+    cout << "6- Rotate Image" << endl;
+    cout << "7- Detect Image Edges" << endl;
+    cout << "8- Enlarge Image" << endl;
+    cout << "9- Shrink Image" << endl;
+    cout << "a- Mirror 1/2 Image" << endl;
+    cout << "b- Shuffle Image" << endl;
+    cout << "c- Blur Image" << endl;
+    cout << "d- Crop Image" << endl;
+    cout << "e- Skew Image Right" << endl;
+    cout << "f- Skew Image Up" << endl;
+    cout << "s- Save the image to a file" << endl;
+    cout << "0- Exit" << endl;
+
     int option = -1;
+    do {
+        cin >> option;
+        switch (option) {
+            case 1:
+                Filter1();
+                break;
+            case 2:
+                Filter2();
+                break;
+            case 3:
+                Filter3();
+                break;
+            case 4:
+                Filter4();
+                break;
+            case 5:
+                Filter5();
+                break;
+            case 6:
+                Filter6();
+                break;
+            default:
+                break;
+        }
+    } while (option != 0);
 }
