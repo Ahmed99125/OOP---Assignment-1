aa89f47c1e8e7cae6ce9ca35c8e4182db90d534e
Ahmed99125 <at99125@gmail.com>
2023-10-09 18:05:50 +0300

    last commit

--- CS213-2023-20220013-20220309-2022416-A1-Part1.cpp
+++ CS213-2023-20220013-20220309-2022416-A1-Part1.cpp
@@ -1,3 +1,12 @@
+// FCAI – OOP Programming – 2023 - Assignment 1
+// Program Name:            CS213-2023-YourID-YourID-YourID-A1-Part1.cpp
+// Last Modification Date:	9/10/2023
+// Author1 and ID and Group:    Ahmed tamer 20220013 at99125@gmail.com
+// Author2 and ID and Group:	Mohamed mostafa 20220309 mohamed.5.20047@gmail.com
+// Author3 and ID and Group:	youssef nasser 2022416 y.shoman500@gmail.com
+// Teaching Assistant:		xxxxx xxxxx
+// Purpose:..........
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -30,6 +39,7 @@ void FilterGreyB();
 void FilterGreyC();
 void FilterGreyD();
 void FilterGreyE();
+void FilterGreyF();
 
 void blur();
 
@@ -37,26 +47,34 @@ int main() {
     display();
 }
 
-
+// load the image
 int loadGreyImage(unsigned char img[][SIZE]) {
     cout << "Please enter file name of the image to process:" << endl;
     string fileName;
     cin >> fileName;
     fileName += ".bmp";
+
+    // get the path of the cpp file
     char cwd[PATH_MAX];
     fileName = strcat(getcwd(cwd, sizeof(cwd)), "/") + fileName;
+
     return 1 - readGSBMP(fileName.c_str(), img);
 }
 
+// save the image
 void saveGreyImage() {
+    cout << "Please enter target file name:" << endl;
     string fileName;
     cin >> fileName;
     fileName += ".bmp";
+
+    // get the path of the cpp file
     char cwd[PATH_MAX];
     fileName = strcat(getcwd(cwd, sizeof(cwd)), "/") + fileName;
     writeGSBMP(fileName.c_str(), image);
 }
 
+// save the image in a tmp folder and show the image immediatly
 void showGrayImage() {
     writeGSBMP(".\\tmp\\img.bmp", image);
     system(".\\tmp\\img.bmp");
@@ -113,13 +131,25 @@ void display() {
             case 'e':
                 FilterGreyE();
                 break;
+            case 'f':
+                FilterGreyF();
+                break;
+            case 's':
+                saveGreyImage();
+                break;
+            case 'l':
+                while (!loadGreyImage(image))
+                    cout << "Please enter a correct file name." << endl;
+                break;
             default:
+                cout << "Please enter valid input." << endl;
                 break;
         }
     } while (option != 0);
 }
 
-void FilterGrey1(){ // Black and White Image
+void FilterGrey1()  { // Black and White Image
+    // if the pixel value is greater than 127 make the pixel white otherwise make it black
     for(int i = 0; i < SIZE; ++i) {
         for(int j = 0; j < SIZE; ++j) {
             if(image[i][j] <= 127)
@@ -132,7 +162,7 @@ void FilterGrey1(){ // Black and White Image
     showGrayImage();
 }
 
-void FilterGrey2(){ // : Invert Image
+void FilterGrey2() { // : Invert Image
     for(int i = 0; i < SIZE; ++i) {
         for(int j = 0; j < SIZE; ++j) {
             image[i][j] = 255 - image[i][j];
@@ -142,7 +172,8 @@ void FilterGrey2(){ // : Invert Image
     showGrayImage();
 }
 
-void FilterGrey3(){ // Merge Images
+void FilterGrey3() { // Merge Images
+    // take input from the user
     cout << "Please enter name of image file to merge with:" << endl;
     while (!loadGreyImage(image2)) {
         cout << "Please enter a correct file name." << endl;
@@ -150,6 +181,7 @@ void FilterGrey3(){ // Merge Images
 
     for(int i = 0; i < SIZE; ++i) {
         for(int j = 0; j < SIZE; ++j) {
+            // take the average of the two images value for each pixel
             image[i][j] = (image[i][j] + image2[i][j]) / 2;
         }
     }
@@ -157,13 +189,16 @@ void FilterGrey3(){ // Merge Images
     showGrayImage();
 }
 
-void FilterGrey4(){ //Flip Image
+void FilterGrey4() { //Flip Image
+    // take input from the user
     cout << "Flip (h)orizontally or (v)ertically ?" << endl;
     char c; cin >> c;
     while (c != 'h' && c != 'v') {
         cout << "please enter valid input." << endl;
         cin >> c;
     }
+
+    // swap the two pixels
     if (c == 'h') {
         for(int i=0; i<SIZE; ++i) {
             for(int j=0; j<SIZE/2; ++j) {
@@ -182,7 +217,8 @@ void FilterGrey4(){ //Flip Image
     showGrayImage();
 }
 
-void FilterGrey5(){ // Darken and Lighten Image
+void FilterGrey5() { // Darken and Lighten Image
+    // take the input from the user
     cout << "Do you want to (d)arken or (l)ighten?" << endl;
     char c; cin >> c;
     while (c != 'd' && c != 'l') {
@@ -194,7 +230,7 @@ void FilterGrey5(){ // Darken and Lighten Image
         for(int j=0; j<SIZE; ++j) {
             // plus for ligher
             if (c == 'l')
-                image[i][j] += (SIZE-image[i][j])/2;
+                image[i][j] += (255-image[i][j])/2;
             // minus for darker
             else if (c == 'd')
                 image[i][j] -= image[i][j]/2;
@@ -204,7 +240,8 @@ void FilterGrey5(){ // Darken and Lighten Image
     showGrayImage();
 }
 
-void FilterGrey6(){ // Rotate Image
+void FilterGrey6() { // Rotate Image
+    // take the input from the user
     cout << "Rotate (90), (180) or (270) degrees?" << endl;
     int x; cin >> x;
     while (x != 90 && x != 180 && x != 270) {
@@ -212,7 +249,7 @@ void FilterGrey6(){ // Rotate Image
         cin >> x;
     }
 
-
+    // rotate the image 90 deg
     auto rotate = []() {
         unsigned char tmp[SIZE][SIZE];
 
@@ -258,7 +295,13 @@ void FilterGrey7() { // Edge Detector
 
     // Sobel Edge Detector
 
-    int multiply[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    // convolutional martices
+    int multiplyX[3][3] = {{-1, 0, 1},
+                           {-2, 0, 2},
+                           {-1, 0, 1}};
+    int multiplyY[3][3] = {{-1, -2, -1},
+                           {0, 0, 0},
+                           {1, 2, 1}};
 
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -267,11 +310,14 @@ void FilterGrey7() { // Edge Detector
                 for (int y = -1; y < 2; y++) {
                     if (i+x < 0 || i+x >= SIZE || j+y < 0 || j+y >= SIZE)
                         continue;
-                    gx += image[i+x][j+y] * multiply[x+1][y+1];
-                    gy += image[i+x][j+y] * multiply[y+1][x+1];
+                    gx += image[i+x][j+y] * multiplyX[x+1][y+1];
+                    gy += image[i+x][j+y] * multiplyY[x+1][y+1];
                 }
             }
+            // the magnitude = sqrt(gx^2 + gy^2)
             gx *= gx, gy *= gy;
+            // threshold = 180
+            // if the magnitude > 180 make the pixel black otherwise make it white
             if ((int)(round(sqrt(gx+gy))) > 180)
                 tmp[i][j] = 0;
             else
@@ -290,14 +336,20 @@ void FilterGrey7() { // Edge Detector
 
 void FilterGrey8() { //Enlarge image
     int x = 0;
+    // take the input from the user
     cout << "Enter the quarter to enlarge from 1, 2, 3, 4: " << endl;
     cin >> x;
     while(x != 1 && x != 2 && x != 3 && x != 4) {
         cout << "Enter a valid input.\n";
         cin >> x;
     }
+
     unsigned char tmp[SIZE][SIZE];
+
+    // every 4 pixels = 1 pixel
+
     if(x == 1) {
+        // enlarge the first quarter
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
                 tmp[i * 2][j * 2] = image[i][j];
@@ -306,7 +358,9 @@ void FilterGrey8() { //Enlarge image
                 tmp[i * 2 + 1][j * 2 + 1] = image[i][j];
             }
         }
-    } else if(x == 2) {
+    }
+    else if(x == 2) {
+        // enlarge the second quarter
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=SIZE / 2; j<SIZE; ++j) {
                 tmp[i * 2][(j - SIZE / 2) * 2] = image[i][j];
@@ -315,7 +369,9 @@ void FilterGrey8() { //Enlarge image
                 tmp[i * 2 + 1][(j - SIZE / 2) * 2 + 1] = image[i][j];
             }
         }
-    } else if(x == 3) {
+    }
+    else if(x == 3) {
+        // enlarge the third quarter
         for(int i=SIZE / 2; i<SIZE; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
                 tmp[(i - SIZE / 2) * 2][j * 2] = image[i][j];
@@ -324,7 +380,9 @@ void FilterGrey8() { //Enlarge image
                 tmp[(i - SIZE / 2) * 2 + 1][j * 2 + 1] = image[i][j];
             }
         }
-    } else {
+    }
+    else {
+        // enlarge the fourth quarter
         for(int i=SIZE / 2; i<SIZE; ++i) {
             for(int j=SIZE / 2; j<SIZE; ++j) {
                 tmp[(i - SIZE / 2) * 2][(j - SIZE / 2) * 2] = image[i][j];
@@ -346,13 +404,16 @@ void FilterGrey8() { //Enlarge image
 
 void FilterGrey9() { // Shrink image
     int x = 0;
+    // take the input from the user
     cout << "Enter the ratio of shrink from 2, 3, 4: " << endl;
     cin >> x;
     while(x != 2 && x != 3 && x != 4) {
         cout << "Enter a valid input.\n";
         cin >> x;
     }
+
     if(x == 2) {
+        // every pixel = the average of its 4 adjacent pixels
         unsigned char tmp[SIZE / 2][SIZE / 2];
         for(int i=0; i<SIZE; i += 2) {
             for(int j=0; j<SIZE; j += 2) {
@@ -369,7 +430,9 @@ void FilterGrey9() { // Shrink image
                 image[i][j] = tmp[i][j];
             }
         }
-    } else if(x == 3) {
+    }
+    else if(x == 3) {
+        // every pixel = the average of its 9 adjacent pixels
         unsigned char tmp[SIZE / 3][SIZE / 3];
         for(int i=1; i<SIZE; i += 3) {
             for(int j=1; j<SIZE; j += 3) {
@@ -386,7 +449,9 @@ void FilterGrey9() { // Shrink image
                 image[i][j] = tmp[i][j];
             }
         }
-    } else {
+    }
+    else {
+        // every pixel = the average of its 16 adjacent pixels
         unsigned char tmp[SIZE / 4][SIZE / 4];
         for(int i=0; i<SIZE; i += 4) {
             for(int j=0; j<SIZE; j += 4) {
@@ -410,12 +475,16 @@ void FilterGrey9() { // Shrink image
 
 void FilterGreyA() { // Mirror 1/2 image
     int x = 0;
+    // take the input from the user
     cout << "Enter the part you want to mirror 1(top-down), 2(down-top), 3(left-right), 4(right-left): " << endl;
     cin >> x;
     while(x != 1 && x != 2 && x != 3 && x != 4) {
         cout << "Enter a valid input.\n";
         cin >> x;
     }
+
+    // make half of the image = the other half
+
     if(x == 1) {
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=0; j<SIZE; ++j) {
@@ -447,56 +516,64 @@ void FilterGreyA() { // Mirror 1/2 image
 
 void FilterGreyB() { // Shuffle Image
     int w, x, y, z;
+    // take the input from the user
     cout << "New order of quarters ?" << endl;
     cin >> w >> x >> y >> z;
     while(w < 1 || w > 4 || x < 1 || x > 4 || y < 1 || y > 4 || z < 1 || z > 4) {
         cout << "Enter a valid input.\n";
         cin >> w >> x >> y >> z;
     }
+
     unsigned char tmp1[SIZE / 2][SIZE / 2];
     unsigned char tmp2[SIZE / 2][SIZE / 2];
     unsigned char tmp3[SIZE / 2][SIZE / 2];
     unsigned char tmp4[SIZE / 2][SIZE / 2];
+
+    // save quarters in tmp arrays
     for(int i=0; i<SIZE / 2; ++i) {
         for(int j=0; j<SIZE / 2; ++j) {
             tmp1[i][j] = image[i][j];
         }
     }
-    for(int i=0; i<SIZE / 2; ++i) {
+    for (int i=0; i<SIZE / 2; ++i) {
         for(int j=SIZE / 2; j<SIZE; ++j) {
             tmp2[i][j - SIZE / 2] = image[i][j];
         }
     }
-    for(int i=SIZE / 2; i<SIZE; ++i) {
+    for (int i=SIZE / 2; i<SIZE; ++i) {
         for(int j=0; j<SIZE / 2; ++j) {
             tmp3[i - SIZE / 2][j] = image[i][j];
         }
     }
-    for(int i=SIZE / 2; i<SIZE; ++i) {
+    for (int i=SIZE / 2; i<SIZE; ++i) {
         for(int j=SIZE / 2; j<SIZE; ++j) {
             tmp4[i - SIZE / 2][j - SIZE / 2] = image[i][j];
         }
     }
 
-    if(w == 1) {
+    // put the new quarter in the first place in the image
+    if (w == 1) {
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
                 image[i][j] = tmp1[i][j];
             }
         }
-    } else if(w == 2) {
+    }
+    else if (w == 2) {
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
                 image[i][j] = tmp2[i][j];
             }
         }
-    } else if(w == 3) {
+    }
+    else if (w == 3) {
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
                 image[i][j] = tmp3[i][j];
             }
         }
-    } else if(w == 4) {
+    }
+    else if (w == 4) {
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
                 image[i][j] = tmp4[i][j];
@@ -504,6 +581,7 @@ void FilterGreyB() { // Shuffle Image
         }
     }
 
+    // put the new quarter in the second place in the image
     if(x == 1) {
         for(int i=0; i<SIZE / 2; ++i) {
             for(int j=SIZE / 2; j<SIZE; ++j) {
@@ -530,6 +608,7 @@ void FilterGreyB() { // Shuffle Image
         }
     }
 
+    // put the new quarter in the third place in the image
     if(y == 1) {
         for(int i=SIZE / 2; i<SIZE; ++i) {
             for(int j=0; j<SIZE / 2; ++j) {
@@ -556,6 +635,7 @@ void FilterGreyB() { // Shuffle Image
         }
     }
 
+    // put the new quarter in the fourth place in the image
     if(z == 1) {
         for(int i=SIZE / 2; i<SIZE; ++i) {
             for(int j=SIZE / 2; j<SIZE; ++j) {
@@ -595,6 +675,7 @@ void FilterGreyC() { // Blur Image
 
 void FilterGreyD() { // Crop Filter
     int x, y, l, w;
+    // take the input from the user
     cout << "Please enter x y l w: " << endl;
     cin >> x >> y >> l >> w;
     while (x < 0 || x > 256 || y < 0 || y > 256 || l < 0 || l > 256 || w < 0 || w > 256) {
@@ -603,7 +684,6 @@ void FilterGreyD() { // Crop Filter
     }
 
     unsigned char tmp[SIZE][SIZE];
-    cout << SIZE << endl;
 
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -611,12 +691,14 @@ void FilterGreyD() { // Crop Filter
         }
     }
 
+    // start from row x and column y to row x+l and column y+w
     for (int i = x; i < x+l; i++) {
         for (int j = y; j < y+w; j++) {
             tmp[i][j] = image[i][j];
         }
     }
 
+    // copy the result to the image
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             image[i][j] = tmp[i][j];
@@ -626,11 +708,15 @@ void FilterGreyD() { // Crop Filter
     showGrayImage();
 }
 
-void FilterGreyE() { // Skew Image
-    int x;
+void FilterGreyE() { // Skew Image Right
+    int angle;
+    // take the input from the user
     cout << "Please enter degree to skew right:" << endl;
-    cin >> x;
-    x = ((x%360)+360) % 360;
+    cin >> angle;
+    while (angle < 0 || angle > 45) {
+        cout << "Please enter valid input." << endl;
+        cin >> angle;
+    }
 
     unsigned char tmp[SIZE][SIZE];
 
@@ -640,27 +726,100 @@ void FilterGreyE() { // Skew Image
         }
     }
 
-    float ratio = 1 / cos(90-x);
-    float space = abs( atan(x));
-    float shrink = 3, remainder = 0;
-
     for (int i = 0; i < SIZE; i++) {
-        int j = 0;
-        while (j  < SIZE) {
+        // compute the white places on the left of the image and on its right
+        int left = (SIZE - i) * tan(angle * M_PI / 180.0),
+            right = (i+1) * tan(angle * M_PI / 180.0),
+            w = left;
+
+        // compute the shrink ratio
+        double shrink = (double) SIZE / (SIZE - (left + right)),
+            remainder = 0;
+
+        // move by the shrink ratio in each step
+        for (int j = 0; j < SIZE; j+=floor(shrink + remainder)) {
+            // compute the average
             int avg = 0;
             for (int k = 0; k < floor(shrink + remainder); k++) {
                 avg += image[i][j+k];
             }
-            tmp[i][(int)floor(j/(shrink + remainder))] = avg / (shrink + remainder);
-            j++;
+            // if the pixels of the skewed image exceeded the computed width break
+            if (w > SIZE - right)
+                break;
 
-            if (floor(shrink + remainder) > shrink)
-                remainder -= floor(shrink + remainder) - shrink;
+            // assign the average value to the image
+            tmp[i][w++] = avg / floor(shrink + remainder);
+
+            // update the remainder
+            if (floor(shrink + remainder) > floor(shrink))
+                remainder -= (1 - (shrink - floor(shrink)));
             else
                 remainder += shrink - floor(shrink);
         }
     }
 
+    // copy the result to the image
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            image[i][j] = tmp[i][j];
+        }
+    }
+
+    showGrayImage();
+}
+
+void FilterGreyF() { // Skew Image Up
+    int angle;
+    // take the input from the user
+    cout << "Please enter degree to skew Up:" << endl;
+    cin >> angle;
+    while (angle < 0 || angle >= 45) {
+        cout << "Please enter valid input." << endl;
+        cin >> angle;
+    }
+
+    unsigned char tmp[SIZE][SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            tmp[i][j] = 255;
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        // compute the white places on the left of the image and on its right
+        int left = (SIZE - i) * tan(angle * M_PI / 180.0),
+                right = (i+1) * tan(angle * M_PI / 180.0),
+                w = left;
+
+        // compute the shrink ratio
+        double shrink = (double) SIZE / (SIZE - (left + right)),
+                remainder = 0;
+
+        // move by the shrink ratio in each step
+        for (int j = 0; j < SIZE; j+=floor(shrink + remainder)) {
+            // compute the average
+            int avg = 0;
+            for (int k = 0; k < floor(shrink + remainder); k++) {
+                avg += image[j][i+k];
+            }
+
+            // if the pixels of the skewed image exceeded the computed width break
+            if (w > SIZE - right)
+                break;
+
+            // assign the average value to the image
+            tmp[w++][i] = avg / floor(shrink + remainder);
+
+            // update the remainder
+            if (floor(shrink + remainder) > floor(shrink))
+                remainder -= (1 - (shrink - floor(shrink)));
+            else
+                remainder += shrink - floor(shrink);
+        }
+    }
+
+    // copy the result to the image
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             image[i][j] = tmp[i][j];
@@ -676,6 +835,7 @@ void blur() {
     int dx[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
     int dy[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
 
+    // take the average of the 9 adjacent pixels
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             int avg = 0;
@@ -714,5 +874,6 @@ void showMenu() {
     cout << "e- Skew Image Right" << endl;
     cout << "f- Skew Image Up" << endl;
     cout << "s- Save the image to a file" << endl;
+    cout << "l- Load another image" << endl;
     cout << "0- Exit" << endl;
 }
